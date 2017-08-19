8b85149a36e020e4f592226a1735dc92ad79bc88
Francisco Fernandez Lopez <fran_kmen@hotmail.com>
2017-08-18 21:25:56 -0600

    Configurando Proyecto. Configurando BundleConfig

--- ProyectoWebAmilcarComercial/AmilcarComercial/App_Start/BundleConfig.cs
+++ ProyectoWebAmilcarComercial/AmilcarComercial/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace AmilcarComercial
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                       "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -19,13 +19,21 @@ namespace AmilcarComercial
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+            bundles.Add(new ScriptBundle("~/bundles/materialize").Include(
+                      "~/Scripts/materialize/materialize.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/libraries").Include(
+                      "~/Scripts/pace.js",
+                      "~/Scripts/jquery.mCustomScrollbar.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/materialize/css/materialize.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/libraries").Include(
+                      "~/Content/pace/pace-theme-flat-top.css",
+                      "~/Content/jquery.mCustomScrollbar.css"));
         }
     }
 }
